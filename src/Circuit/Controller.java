2be20ed0a6927a437442274edcf62ceabb2e6cc9
Roy Chavarría <roychav99@gmail.com>
2019-09-08 15:28:21 -0600

    Commit #1
    Se paso el proyecto de Swing a JavaFX por comodidad

---
+++ src/Circuit/Controller.java
@@ -0,0 +1,4 @@
+package Circuit;
+
+public class Controller {
+}
---
+++ src/Circuit/Main.java
@@ -0,0 +1,24 @@
+package Circuit;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Circuit.fxml"));
+        Platform.isImplicitExit();
+        primaryStage.setTitle("Circuit Designer");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1100, 630));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
